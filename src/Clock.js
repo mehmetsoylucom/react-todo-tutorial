d9e9638e4c240d4311902066a6dcd7bfdab935a4
mehmetsoylucom <mehmet@soylu.net>
2019-04-30 00:38:16 +0300

    component classes

--- src/Clock.js
+++ src/Clock.js
@@ -4,13 +4,19 @@ import React from 'react';
 Bu component saati gosteriyor. index.js icinde render edilen fonksiyon
 her saniyede bir kez calisip DOM'un degisen tek kismi olan saati guncelliyor.
  */
-function Clock(props) {
-    return (
-        <div>
-            <h1>Merhaba Dünya!</h1>
-            <h2>Saat şu anda {props.date.toLocaleTimeString()}</h2>
-        </div>
-    );
+
+/*
+Component artik bir class ve ES6 class yapisini kullaniyor.
+Artik class kullandigimizdan state ve lifecycle da kullanabiliriz.
+ */
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Saat şu anda {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
 export default Clock;
