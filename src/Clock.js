fbb87d671ef4159040f47ed6ff240940a2ba923b
mehmetsoylucom <mehmet@soylu.net>
2019-04-30 00:50:47 +0300

    mount, unmount ve setstate

--- src/Clock.js
+++ src/Clock.js
@@ -10,24 +10,47 @@ Component artik bir class ve ES6 class yapisini kullaniyor.
 Artik class kullandigimizdan state ve lifecycle da kullanabiliriz.
  */
 class Clock extends React.Component {
-    /**
-     * Constructor ile React.Component propslarini cagirdikten sonra
-     * class bazinda kullanilacak state veri deposunu olusturuyoruz.
-     *
-     * @param props
-     */
     constructor(props) {
         super(props);
         this.state = {date: new Date()};
     }
 
+    /**
+     * Mount ve Unmount fonksiyonlari lifecycle hooks olarak gecer
+     */
+    componentDidMount() {
+        // Component oluşturulduğunda çalışacak fonksiyon
+        this.timerID = setInterval(
+            // () => this.tick() kullanımı ES6 ile gelen kısa fonksiyon kullanımıdır.
+            () => this.tick(), // Class icinde cagrilan bir function
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        // Component kaldırıldığında çalışacak fonksiyon
+        clearInterval(this.timerID);
+    }
+
+    /**
+     * Fonksiyon tanımlarken başına "function" yazmadığımıza dikkat edin.
+     * React componentleri içerisinde fonksiyon tanımlamak istediğimizde direkt isim vererek yazıyoruz.
+     */
+    tick() {
+        // setState() çağrısı sayesinde React, statin değiştiğini bilir ve ekranda
+        // ne olması gerektiğini öğrenmek için tekrar render() eder
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
             <div>
-                {/* bu satirda state kullanimi ile date cagrilmis durumda*/}
                 <h2>Saat şu anda {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
     }
 }
+
 export default Clock;
