eb159e13947052a62884a9e40a527960c09bd46d
mehmetsoylucom <mehmet@soylu.net>
2019-04-29 20:28:16 +0300

    do not change the props, just read and use

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+/*
+Componentleri küçük parçalara bölmek başlangıçta gereksiz bir iş
+yada zaman kaybı gibi gözükebilir, ancak daha büyük uygulamalarda
+tekrar kullanılabilir component paletine sahip olmak önemlidir.
+ */
 function Welcome(props) {
+    // Burada gelen props uzerinde bir islem yapmamaliyiz.
+    // Islem yapilacaksa sadece return kisminda yapilmalidir.
+    // Yani gelen parametreler degismemelidir
+    // props sadece okuma iznine sahiptir gibi dusunmeliyiz.
     return (
         <span>
             Merhaba, {props.name}
